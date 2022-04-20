860dff72c8f1c0727f95a57269916df6eef4570e
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2022-04-20 14:21:52 +0600

    Using Fetch API in NewwItem

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -8,7 +8,7 @@ export class NewsItemComponent extends Component {
     return (
       <div className="my-3">
        <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={imageUrl?imageUrl:"https://images.moneycontrol.com/static-mcnews/2022/03/fandosensexniftyderivative-770x433.jpg"} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}..</h5>
             <p className="card-text">{description}..</p>
