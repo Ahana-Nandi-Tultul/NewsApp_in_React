1fb4f1ebe2b6cd29e4aae0bdf8ae5d78cc89b757
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2022-04-21 20:49:11 +0600

    Fetching News Category Wise

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import NewsComponent from './components/NewsComponent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
-  c="Ahana";
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <NewsComponent pageSize={5} country={'in'} category={"science"}/>
+        
+            <Switch>
+              {/* <Route exact path="/about" element={<About/>}/> */}
+              <Route exact path="/business"><NewsComponent key="business"  pageSize={5} country={'in'} category={"business"}/></Route>
+              <Route exact path="/entertainment"><NewsComponent key="entertainment"  pageSize={5} country={'in'} category={"entertainment"}/></Route>
+              <Route exact path="/general"><NewsComponent key="general"  pageSize={5} country={'in'} category={"general"}/></Route>
+              <Route exact path="/health"><NewsComponent  key="health"p ageSize={5} country={'in'} category={"health"}/></Route>
+              <Route exact path="/science"><NewsComponent  key="science"p ageSize={5} country={'in'} category={"science"}/></Route>
+              <Route exact path="/sports"><NewsComponent key="sports"  pageSize={5} country={'in'} category={"sports"}/></Route>
+              <Route exact path="/technology"><NewsComponent key="technology"  pageSize={5} country={'in'} category={"technology"}/></Route>
+              </Switch>
+        </Router>
       </div>
     )
   }
